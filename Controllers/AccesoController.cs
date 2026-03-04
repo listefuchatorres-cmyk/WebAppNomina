72fbf1ac522c287eecee81288cd91b1db46baa43
Estefani Torres <listefuchatorres@gmail.com>
2026-03-03 23:08:37 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/AccesoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppNomina.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Autenticar()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+       
+    }
+}
