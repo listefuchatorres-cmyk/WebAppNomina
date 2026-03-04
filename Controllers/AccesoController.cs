9c236ac0dc3d2abebcb2ded8763a4ba9929fd3cd
Isaac-Morales-ism <isaac.morales@ister.edu.ec>
2026-03-04 01:36:05 -0500

    modelos

--- Controllers/AccesoController.cs
+++ Controllers/AccesoController.cs
@@ -6,18 +6,84 @@ using System.Web.Mvc;
 
 namespace WebAppNomina.Controllers
 {
-    public class AccesoController : Controller
+    public class accesoController : Controller
     {
-        // GET: Acceso
-        public ActionResult Autenticar()
+        // GET: acceso
+        public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Registro()
+        // GET: acceso/Details/5
+        public ActionResult Details(int id)
         {
             return View();
         }
-       
+
+        // GET: acceso/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: acceso/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: acceso/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: acceso/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: acceso/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: acceso/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
