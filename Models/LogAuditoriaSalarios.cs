467f01979c91c7f4271632952b5ef457d7557e73
Michael Becerra <michael10becerra@gmail.com>
2026-03-04 03:01:30 -0500

    Login

---
+++ Models/LogAuditoriaSalarios.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class LogAuditoriaSalarios
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Usuario { get; set; }
+
+    public DateTime FechaActualizacion { get; set; }
+
+    public string DetalleCambio { get; set; }
+
+    public decimal Salario { get; set; }
+
+    public int EmpNo { get; set; }
+}
